67402b9f96f7cdf79399ef22633164931c96f159
Jacson R. Sasse <jacson.sasse@hotmail.com>
2024-01-16 00:45:18 -0300

    Feat: added router

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,12 @@
-import Login from "./pages/Login";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./Router";
 
 function App() {
-  return <Login></Login>;
+  return (
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  );
 }
 
 export default App;
