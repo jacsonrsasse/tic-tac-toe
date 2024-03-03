90a3b943ab32a9685ed3ad04d115f7a801a28e84
Jacson R. Sasse <jacson.sasse@hotmail.com>
2024-03-03 20:40:52 -0300

    Feat: add Dockerfile in both projects

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const cwd = process.cwd();
+  const env = { ...loadEnv(mode, cwd, "VITE_") };
+
+  const serverConfig = {
+    host: true,
+    port: Number(env.VITE_SERVER_PORT),
+    strictPort: true,
+  };
+
+  return {
+    plugins: [react()],
+    server: serverConfig,
+  };
+});
